bf31eaab8f5688082d2af14f6a8a9ed9abf2aed0
Prince Patel <ext_prpatel@falabella.cl>
2023-01-13 00:18:06 +0530

    helper

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,8 @@
 <div class="content-header">
     <div class="content-header-title">Dashboard</div>
 </div>
-<input class="date-picker" />
+<div class="content-body bg-white">
+    <input type="text" class="date-picker">
+</div>
 
 <?php include_once './includes/footer.php'; ?>
