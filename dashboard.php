2ed93cf1ea8c93f05e55b44d1f690aac114db411
Prince Patel <ext_prpatel@falabella.cl>
2023-01-12 18:37:45 +0530

    header

---
+++ dashboard.php
@@ -0,0 +1,5 @@
+<?php include_once './includes/header.php'; ?>
+
+<div>Page Container</div>
+
+<?php include_once './includes/footer.php'; ?>
---
+++ includes/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../assets/scss/styles.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="page-layout">
+
+        <header class="page-header">
+            <div class="page-header-item page-header-main">
+                <a href="#" class="page-header-logo">
+                    <img src="../assets/images/logo.png" alt="">
+                </a>
+            </div>
+            <div class="page-header-item page-header-content">
+                <div class="page-header-content-item">
+                    <div class="profile-container">
+                        <div class="avatar">JM</div>
+                        <div class="profile-name">Jewel Majumder</div>
+                        <div class="profile-id">Customer ID : 263248</div>
+                    </div>
+                </div>
+                <div class="page-header-content-item">
+                    <div class="page-header-actions">
+                        <button class="btn">
+                            <span class="material-icons-outlined">email</span>
+                            <span>Create</span>
+                        </button>
+                        <button class="btn btn-text">
+                            <span class="material-icons-outlined">notifications</span>
+                            <div class="badge badge-floating badge-secondary">9+</div>
+                        </button>
+                        <button class="btn">
+                            <span class="material-icons-outlined">logout</span>
+                            <span>Log out</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <aside class="page-sider">Sidebar</aside>
+
+        <div class="page-content">
