f1c63ee08fe82e935bbbc48a7d310837063c3955
Earth Patel <ep.8905685003@gmail.com>
2023-01-21 00:12:41 +0530

    tender-notice.php

--- tender-notice.php
+++ tender-notice.php
@@ -47,7 +47,229 @@
     
 </div>
 <div class="content-body bg-white">
+    <div class="row">
+        <div class="col">
+            
+            <div class="box-gy-6">
+                <div class="box-gy-2">
+                    <div class="font-weight-sm font-lg text-muted">Descriptions</div>
+                    <div>The WEA is seeking to secure a supply chain of providers to support delivery of Multiply funded activity in the Greater Manchester Combined Authority area. We are inviting expressions of interest (EOI) from organisations that wish to deliver GMCA Multiply courses as part of our supply chain. We wish to hear from organisations with..... SEE MORE.</div>
+                </div>
     
+                <div class="box-gy-4">
+                    <div class="row">
+                        <div class="col-6 box-gy-1">
+                            <div class="font-weight-sm text-muted">Tender Type</div>
+                            <span class="material-icons-outlined icon-xs">trending_up</span>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Region</div>
+                            <div class="text-dark">UK</div>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Tenderbase Id</div>
+                            <div class="text-dark">0512200171</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 box-gy-1">
+                            <div class="font-weight-sm text-muted">Procedure</div>
+                            <span class="text-dark">Restricted</span>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Suitable for SME</div>
+                            <div class="text-dark">Yes</div>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Nationwide</div>
+                            <div class="text-dark">False</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 box-gy-1">
+                            <div class="font-weight-sm text-muted">Notice Type</div>
+                            <span class="text-dark">Contract</span>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Tender Identifier</div>
+                            <div class="text-dark">TPHeat001-DN513110-55308925</div>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Attached documents</div>
+                            <div class="text-dark">Notice_3.pdf (Download)</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row-divider"></div>
+
+                <div>
+                    <div class="row">
+                        <div class="col box-gy-2">
+                            <div class="font-weight-sm font-lg text-muted">Buyer Info :</div>
+                            <div class="box-gy-1">
+                                <div class="font-lg text-dark">Department for Business, Energy & Industrial Strategy (BEIS)</div>
+                                <div>Address - Oxford Nanopore Technologies Limited, Oxford.</div>
+                                <div>Phone number - 1334444233</div>
+                                <div>
+                                    <span>Email - </span> 
+                                    <span class="text-dark">buyer@email.com</span>
+                                </div>
+                                <div>
+                                    <span>Website - </span>
+                                    <span class="text-dark">buyer.com</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto align-self-end">
+                            <button class="btn btn-outlined-dark">
+                                <span>View profile</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row-divider"></div>
+
+                <div class="box-gy-2">
+                    <div class="font-weight-sm font-lg text-muted">Tender CPV Codes</div>
+                    <div class="d-flex ps-2 align-items-center box-gx-2 text-dark">
+                        <span class="material-icons icon-bullet">lens</span> 
+                        <span>48400000 - Business transaction and personal business software package </span>
+                    </div>
+                    <div class="d-flex ps-2 align-items-center box-gx-2 text-dark">
+                        <span class="material-icons icon-bullet">lens</span> 
+                        <span>48400000 - Business transaction and personal business software package </span>
+                    </div>
+                </div>
+
+                <div class="row-divider"></div>
+                
+                <div class="box-gy-4">
+                    <div class="font-weight-sm text-dark font-xxl">Information about lots</div>
+                    <div class="row">
+                        <div class="col-6 box-gy-1">
+                            <div class="font-weight-sm text-muted">Number of lots:</div>
+                            <span class="text-dark">3</span>
+                        </div>
+                        <div class="col box-gy-1 font-weight-sm">
+                            <div class="text-muted">Total value of the procurement </div>
+                            <div class="text-dark">Value excluding VAT: £256,700,000</div>
+                        </div>
+                        <div class="col-auto box-gy-1 font-weight-sm">
+                            <button class="btn btn-outlined-primary">
+                                <span>See Lot Info</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row-divider"></div>
+
+                <div>
+                    <div class="font-weight-sm font-lg text-muted">Possible competitors</div>
+                    <ol class="box-gy-5">
+                        <li>
+                            <div class="box-gy-3">
+                                <div class="row">
+                                    <div class="col-20">
+                                        <div class="box-gy-3">
+                                            <div class="box-gy-1">
+                                                <div class="d-flex box-gx-1 align-items-center text-muted">
+                                                    <span class="material-icons-outlined icon-sm">room</span>
+                                                    <span class="font-weight-sm">United Kingdom</span>
+                                                </div>
+                                                <div class="text-dark font-lg">Framework - Multiple Operators As Listed In Section Vi</div>
+                                                <div class="font-weight-sm text-muted">Framework - Multiple Operators as listed in Section VI</div>
+                                            </div>
+                                            <div class="box-gy-1">
+                                                <div>Last Awarded Tender</div>
+                                                <div class="text-secondary">S/10353 National Framework Apprenticeship Training & End Point Assessment, Tranche 1 March 2021</div>
+                                                <div>Date - 06/04/2021</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-8 box-gy-1 font-weight-sm">
+                                        <div class="text-muted">Total Awarded Tenders</div>
+                                        <div class="text-dark">1</div>
+                                    </div>
+                                    <div class="col box-gy-1 font-weight-sm">
+                                        <div class="text-muted">Total Awarded Price</div>
+                                        <div class="text-dark">99999999</div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-8 box-gy-1 font-weight-sm">
+                                        <div class="text-muted">How Frequently Awarded</div>
+                                        <div class="text-dark">Once in 2 years</div>
+                                    </div>
+                                    <div class="col box-gy-1 font-weight-sm">
+                                        <div class="text-muted">Worked with Buyer</div>
+                                        <div class="text-dark">NO</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <button class="btn btn-outlined-dark">
+                                            <span>Show Less</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>    
+                        </li>
+                        <li>
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <div class="box-gy-3">
+                                        <div class="box-gy-1">
+                                            <div class="d-flex box-gx-1 align-items-center text-muted">
+                                                <span class="material-icons-outlined icon-sm">room</span>
+                                                <span class="font-weight-sm">United Kingdom</span>
+                                            </div>
+                                            <div class="text-dark font-lg">Framework - Multiple Operators As Listed In Section Vi</div>
+                                            <div class="font-weight-sm text-muted">Framework - Multiple Operators as listed in Section VI</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-outlined-dark">
+                                        <span>More info</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <div class="box-gy-3">
+                                        <div class="box-gy-1">
+                                            <div class="d-flex box-gx-1 align-items-center text-muted">
+                                                <span class="material-icons-outlined icon-sm">room</span>
+                                                <span class="font-weight-sm">United Kingdom</span>
+                                            </div>
+                                            <div class="text-dark font-lg">Framework - Multiple Operators As Listed In Section Vi</div>
+                                            <div class="font-weight-sm text-muted">Framework - Multiple Operators as listed in Section VI</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-outlined-dark">
+                                        <span>More info</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </li>
+                    </ol>
+                </div>
+
+                <div class="row-divider"></div>
+
+            </div>
+        </div>
+        <div class="col-10"></div>
+    </div>
 </div>
 
 <?php include_once './includes/footer.php'; ?>
