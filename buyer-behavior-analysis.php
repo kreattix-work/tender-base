a3468a29352a69174de7e70f746d61a5787b2fdc
Earth Patel <ep.8905685003@gmail.com>
2023-01-21 16:07:55 +0530

    2 pages

--- buyer-behavior-analysis.php
+++ buyer-behavior-analysis.php
@@ -36,7 +36,7 @@
     <div class="card">
         <div class="card-body d-flex justify-content-between">
             <div class="card-filter-container">
-                <button class="btn btn-text btn-outlined-dark p-0 no-hover">
+                <button class=" btn btn-text btn-outlined-dark p-0 no-hover">
                     <span class="material-icons-outlined">filter_alt</span>
                     <span>Filter</span>
                 </button>
@@ -58,19 +58,265 @@
                 <div class="col-divider"></div>
                 <div class="form-group">
                     <span class="input-icon input-icon material-icons">search</span>
-                    <input type="text" placeholder="Search by town" class="form-control form-control-rounded form-control-sm">
+                    <input type="text" placeholder="Search by tender title" class="form-control form-control-rounded form-control-sm">
                 </div>
             </div>
             <div class="card-filter-container">
-                <button class="btn btn-text p-0 no-hover">
+                <button class=" btn btn-text p-0 no-hover">
                     <span class="material-icons-outlined">sort</span>
-                    <span class="font-weight-sm">Sort by</span>
+                    <span>Sort by</span>
                 </button>
                 <div class="col-divider"></div>
                 <button class="btn btn-text btn-outlined-dark p-0 no-hover">
                     <span class="material-icons">arrow_drop_down</span>
                     <span>Buyer name</span>
                 </button>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="d-flex justify-content-between">
+                <div class="card-filter-container">
+                    <div>
+                        <span class="text-muted font-weight-sm">Search result:</span>
+                        <span class="text-dark">100 Buyers</span>
+                    </div>
+                </div>
+                <div class="card-filter-container">
+                    <div class="font-weight-sm">Suitable for SME</div>
+                    <label class="input-switch">
+                        <input type="checkbox">
+                        <span class="input-switch-indicator"></span>
+                    </label>
+                    <div class="col-divider"></div>
+                    <button class=" btn btn-text p-0 no-hover">
+                        <span class="font-weight-sm">Order by</span>
+                    </button>
+                    <div class="col-divider"></div>
+                    <button class="btn btn-text btn-outlined-dark p-0 no-hover">
+                        <span class="material-icons rotate-90">code</span>
+                        <span class="text-dark">Decending</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body no-border card-body-sm pt-0">
+            <div class="data-card-group">
+                <div class="data-card box-gy-6">
+                    <div class="row gx-0">
+                        <div class="col box-gy-3">
+                            <div class="d-flex align-items-center box-gx-2">
+                                <span class="font-lg text-dark">Apuc Limited On Behalf Of Borders College</span>
+                                <span class="font-weight-sm text-dark highlight-secondary-light">Buyer comparison</span>
+                            </div>
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted icon-sm material-icons-outlined">schedule</span>
+                                    <span class="text-muted font-weight-sm">Last published :</span>
+                                    <span>02/11/2022</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Tender :</span>
+                                    <span>BTA167 Event management centre</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="box-gy-2">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Total Tender :</span>
+                                    <span>50</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Value :</span>
+                                    <span>N/A</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">How frequently :</span>
+                                    <span>Once in a month</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto d-flex box-gx-4">
+                            <button class="btn btn-rounded btn-outlined-secondary">Follow</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-divider my-2"></div>
+                <div class="data-card box-gy-6">
+                    <div class="row gx-0">
+                        <div class="col box-gy-3">
+                            <div class="d-flex align-items-center box-gx-2">
+                                <span class="font-lg text-dark">Apuc Limited On Behalf Of Borders College</span>
+                                <span class="font-weight-sm text-dark highlight-secondary-light">Buyer comparison</span>
+                            </div>
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted icon-sm material-icons-outlined">schedule</span>
+                                    <span class="text-muted font-weight-sm">Last published :</span>
+                                    <span>02/11/2022</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Tender :</span>
+                                    <span>BTA167 Event management centre</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="box-gy-2">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Total Tender :</span>
+                                    <span>50</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Value :</span>
+                                    <span>N/A</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">How frequently :</span>
+                                    <span>Once in a month</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto d-flex box-gx-4">
+                            <button class="btn btn-rounded">Following</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-divider my-2"></div>
+                <div class="data-card box-gy-6">
+                    <div class="row gx-0">
+                        <div class="col box-gy-3">
+                            <div class="d-flex align-items-center box-gx-2">
+                                <span class="font-lg text-dark">Apuc Limited On Behalf Of Borders College</span>
+                                <span class="font-weight-sm text-dark highlight-secondary-light">Buyer comparison</span>
+                            </div>
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted icon-sm material-icons-outlined">schedule</span>
+                                    <span class="text-muted font-weight-sm">Last published :</span>
+                                    <span>02/11/2022</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Tender :</span>
+                                    <span>BTA167 Event management centre</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="box-gy-2">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Total Tender :</span>
+                                    <span>50</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Value :</span>
+                                    <span>N/A</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">How frequently :</span>
+                                    <span>Once in a month</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto d-flex box-gx-4">
+                            <button class="btn btn-rounded btn-outlined-secondary">Follow</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-divider my-2"></div>
+                <div class="data-card box-gy-6">
+                    <div class="row gx-0">
+                        <div class="col box-gy-3">
+                            <div class="d-flex align-items-center box-gx-2">
+                                <span class="font-lg text-dark">Apuc Limited On Behalf Of Borders College</span>
+                                <span class="font-weight-sm text-dark highlight-secondary-light">Buyer comparison</span>
+                            </div>
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted icon-sm material-icons-outlined">schedule</span>
+                                    <span class="text-muted font-weight-sm">Last published :</span>
+                                    <span>02/11/2022</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Tender :</span>
+                                    <span>BTA167 Event management centre</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="box-gy-2">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Total Tender :</span>
+                                    <span>50</span>
+                                </div>
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">Value :</span>
+                                    <span>N/A</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="d-flex align-items-center box-gx-3">
+                                <div class="box-gx-1 d-flex align-items-center">
+                                    <span class="text-muted font-weight-sm">How frequently :</span>
+                                    <span>Once in a month</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto d-flex box-gx-4">
+                            <button class="btn btn-rounded">Following</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-divider my-2"></div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center box-gx-2">
+                    <span class="font-weight-sm">Showing Entities</span>
+                    <select class="form-control form-control-xs">
+                        <option value=" 10" selected>10</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                    <span class="font-weight-sm">Page 1 of 5</span>
+                </div>
+                <div class="d-flex box-gx-2">
+                    <button class="btn btn-outlined btn-sm btn-square" disabled>
+                        <span class="material-icons-outlined">chevron_left</span>
+                    </button>
+                    <button class="btn btn-outlined-secondary btn-sm btn-square">1</button>
+                    <button class="btn btn-outlined btn-sm btn-square">2</button>
+                    <button class="btn btn-outlined btn-sm btn-square">3</button>
+                    <button class="btn btn-outlined btn-sm btn-square">4</button>
+                    <button class="btn btn-outlined btn-sm btn-square">5</button>
+                    <button class="btn btn-outlined-secondary btn-sm btn-square">
+                        <span class="material-icons-outlined">chevron_right</span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
