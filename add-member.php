0c3c539327ac5b400f30f6635ef989947be745df
Prince Patel <prpatel0949@gmail.com>
2023-01-26 23:22:10 +0530

    one more page

--- add-member.php
+++ add-member.php
@@ -8,5 +8,152 @@
         </button>
     </div>
 </div>
-
+<div class="content-body">
+    <div class="card py-5">
+        <form class="card-body box-gy-6">
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <div class="font-lg font-weight-sm">Please entered required information bellow</div>
+                    <button class="btn btn-text text-secondary">
+                        <span class="material-icons">undo</span>
+                        <span class="font-weight-xl font-lg">Reset</span>
+                    </button>
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-6 text-right font-lg text-dark mt-3">
+                    <label for="name">Name</label>
+                </div>
+                <div class="col-12">
+                    <div class="form-group flex-1">
+                        <input type="text" placeholder="Title or Tender id"
+                            class="form-control form-control-lg font-lg font-weight-sm">
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-6 text-right font-lg text-dark mt-3">
+                    <label for="fname">Contact Details</label>
+                </div>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="form-group flex-1">
+                            <input type="text" placeholder="+88"
+                                class="form-control form-control-lg font-lg font-weight-sm">
+                        </div>
+                        <div class="form-group flex-1">
+                            <span class="input-icon input-icon-prefix material-icons-outlined">mail</span>
+                            <input type="text" placeholder="example@email.com"
+                                class="form-control form-control-lg font-lg font-weight-sm">
+                            <span class="form-note">Password will be recieved on email address</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-6 text-right font-lg text-dark mt-3">
+                    <label for=" name">User Role</label>
+                </div>
+                <div class="col-12">
+                    <div class="form-group flex-1">
+                        <input data-trigger-popover="#popoverid-1"
+                            class="form-control form-control-lg font-weight-sm font-lg" value="Manager" readonly />
+                        <div class="popover-dialog" id="popoverid-1">
+                            <div class="popover-content box-gy-3">
+                                <div class="popover-header">
+                                    <div class="text-muted font-weight-sm">Select Role</div>
+                                    <div>
+                                        <button class="btn btn-sm btn-text text-primary p-0 no-hover">
+                                            <span class="material-icons-outlined">add</span>
+                                            <span>Create Role</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="row-divider pb-1"></div>
+                                <div class="popover-body">
+                                    <div class="box-gy-4">
+                                        <label class="form-checkbox-container">
+                                            <input type="checkbox" class="form-checkbox">
+                                            <div class="form-checkbox-indicator">
+                                                <span class="material-icons-outlined">done</span>
+                                            </div>
+                                            <span>Folder Name</span>
+                                        </label>
+                                        <label class="form-checkbox-container">
+                                            <input type="checkbox" class="form-checkbox">
+                                            <div class="form-checkbox-indicator">
+                                                <span class="material-icons-outlined">done</span>
+                                            </div>
+                                            <span>Folder Name</span>
+                                        </label>
+                                        <label class="form-checkbox-container">
+                                            <input type="checkbox" class="form-checkbox">
+                                            <div class="form-checkbox-indicator">
+                                                <span class="material-icons-outlined">done</span>
+                                            </div>
+                                            <span>Folder Name</span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popover-actions">
+                                <button class="btn btn-primary btn-flat flex-1 close-popover">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-6 text-right font-lg text-dark mt-3">
+                    <label for=" name">Assign Department</label>
+                </div>
+                <div class="col-12">
+                    <div class="form-group flex-1">
+                        <select class="form-control  form-control-lg font-weight-sm font-lg">
+                            <option value="6">Food</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-6 text-right font-lg text-dark mt-3">
+                    <labels for="fname">Assign Category & Location</label>
+                </div>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="form-group flex-1">
+                            <span class="input-icon input-icon-as-text p-0 text-secondary">Edit</span>
+                            <input type="text" placeholder="All Categories"
+                                class="font-lg font-weight-sm form-control form-control-lg" />
+                        </div>
+                        <div class="form-group flex-1">
+                            <span class="input-icon input-icon-prefix material-icons-outlined">location_on</span>
+                            <select class="form-control form-control-lg font-lg font-weight-sm">
+                                <option value="6"> 15 selected</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-12">
+                    <div class="row-divider mb-4"></div>
+                    <div class="d-flex justify-content-end box-gx-4">
+                        <button class="btn btn-lg px-5">Cancel</button>
+                        <button class="btn btn-primary btn-lg px-5">Save</button>
+                    </div>
+                </div>
+                <div class="col-6"></div>
+            </div>
+        </form>
+    </div>
+</div>
 <?php include_once './includes/footer.php'; ?>
--- manage-team.php
+++ manage-team.php
@@ -1,12 +1,211 @@
 <?php include_once './includes/header.php'; ?>
 
 <div class="content-header">
-    <div class="content-header-title">Manage Team</div>
-    <div>
-        <button class="btn btn-outlined-primary btn-rounded">
-            <span>Create Team</span>
-        </button>
+    <div class="content-header-title">Manange Team</div>
+    <button class="btn btn-outlined-primary btn-rounded">
+        <span>Create Team</span>
+    </button>
+</div>
+<div class="content-body box-gy-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-filter-container justify-content-between">
+                <div class="">Department Name</div>
+                <div class="">+ Add member</div>
+            </div>
+            <div class="row-divider mt-3"></div>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Member name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Active on</th>
+                            <th>Expire on</th>
+                            <th>Member Role</th>
+                            <th>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="">Actions</div>
+                                    <label class="input-switch">
+                                        <input type="checkbox" checked>
+                                        <span class="input-switch-indicator"></span>
+                                    </label>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-dark">
+                        <tr>
+                            <td>Rajan Raj</td>
+                            <td class="font-weight-sm">rrbusinesstalk@gmail.com</td>
+                            <td class="font-weight-sm">12345667778888</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">Menager</td>
+                            <td>
+                                <div class="d-flex box-gx-3">
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">chat</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">delete</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Rajan Raj</td>
+                            <td class="font-weight-sm">rrbusinesstalk@gmail.com</td>
+                            <td class="font-weight-sm">12345667778888</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">Menager</td>
+                            <td>
+                                <div class="d-flex box-gx-3">
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">chat</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">delete</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Rajan Raj</td>
+                            <td class="font-weight-sm">rrbusinesstalk@gmail.com</td>
+                            <td class="font-weight-sm">12345667778888</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">Menager</td>
+                            <td>
+                                <div class="d-flex box-gx-3">
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">chat</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">delete</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
+<div class="content-body box-gy-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-filter-container justify-content-between">
+                <div class="">Department Name</div>
+                <div class="">+ Add member</div>
+            </div>
+            <div class="row-divider mt-3"></div>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Member name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Active on</th>
+                            <th>Expire on</th>
+                            <th>Member Role</th>
+                            <th>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="">Actions</div>
+                                    <label class="input-switch">
+                                        <input type="checkbox" checked>
+                                        <span class="input-switch-indicator"></span>
+                                    </label>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-dark">
+                        <tr>
+                            <td>Rajan Raj</td>
+                            <td class="font-weight-sm">rrbusinesstalk@gmail.com</td>
+                            <td class="font-weight-sm">12345667778888</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">Menager</td>
+                            <td>
+                                <div class="d-flex box-gx-3">
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">chat</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">delete</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Rajan Raj</td>
+                            <td class="font-weight-sm">rrbusinesstalk@gmail.com</td>
+                            <td class="font-weight-sm">12345667778888</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">Menager</td>
+                            <td>
+                                <div class="d-flex box-gx-3">
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">chat</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">delete</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Rajan Raj</td>
+                            <td class="font-weight-sm">rrbusinesstalk@gmail.com</td>
+                            <td class="font-weight-sm">12345667778888</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">11/11/2022</td>
+                            <td class="font-weight-sm">Menager</td>
+                            <td>
+                                <div class="d-flex box-gx-3">
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">chat</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                                    </button>
+                                    <button class="btn btn-square btn-sm">
+                                        <span class="text-muted icon-xs material-icons-outlined">delete</span>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php include_once './includes/footer.php'; ?>
