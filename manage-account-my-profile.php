2d625132b917387769a40ebbb0eb8c31166a2516
Prince Patel <ext_prpatel@falabella.cl>
2023-01-28 17:13:41 +0530

    change page name

--- manage-account-my-profile.php
+++ manage-account-my-profile.php
@@ -11,5 +11,81 @@
         </button>
     </div>
 </div>
+<div class="content-body box-gx-3">
+    <div class="row">
+        <div class="col-12">
+            <div class="card no-shadow">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <div class="box-gy-4">
+                                <div class="">
+                                    <p class="text-muted my-1 font-weight-sm">Full name</p>
+                                    <p class="my-1">Jewel Majumder</p>
+                                </div>
+                                <div class="">
+                                    <p class="text-muted my-1 font-weight-sm">Email</p>
+                                    <p class="my-1">Jewelultralysis@gmail.com</p>
+                                </div>
+                                <div class="">
+                                    <p class="text-muted my-1 font-weight-sm">Phone</p>
+                                    <p class="my-1">+44 07474823691</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-square btn-sm">
+                                <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="card fill-container no-shadow">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <div class="box-gy-4">
+                                <div class="">
+                                    <p class="text-muted my-1 font-weight-sm">Billing address</p>
+                                    <p>41 Long Lynderswood ,</p>
+                                    <p> Basildon - SS155AY,</p>
+                                    <p>United Kingdom</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-square btn-sm">
+                                <span class="text-muted icon-xs material-symbols-outlined">border_color</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content-body box-gx-3">
+    <div class="card bg-primary-light no-shadow">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="">
+                        <p class="font-weight-sm">Subscription Details</p>
+                        <p class="font-lg text-primary">You have subscribed for Yearly - UK + Ireland Plan With Extra Service. You can always upgrade your plan</p>
+                    </div>
+                </div>
+                <div class="col-12 d-flex justify-content-end align-items-center">
+                    <button class="btn btn-rounded btn-lg btn-primary">
+                        <span class="">Upgrade plan</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php include_once './includes/footer.php'; ?>
